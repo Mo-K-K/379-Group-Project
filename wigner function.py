e4698f1c0c0baa401a50a77a9fdb8825d714cc02
Mo Khan <60451707+Mo-K-K@users.noreply.github.com>
2025-02-25 19:08:04 +0000

    Update wigner function.py
    added comments

--- wigner function.py
+++ wigner function.py
@@ -1,6 +1,6 @@
-def wigner_function(n, theta, p):
+def wigner_function(n, theta, p): #n is the energy level, theta is theta and p is momentum
     def integrand(x):
-        return np.exp(1j * p * x / hbar) * psi(n, theta - x / 2) * np.conj(psi(n, theta + x / 2))
+        return np.exp(1j * p * x / hbar) * psi(n, theta - x / 2) * np.conj(psi(n, theta + x / 2)) #i know functions in functions is a shit practice but it was an easy way forward
     
     integral, _ = quad(lambda x: integrand(x).real, -10, 10)
     return (1 / (np.pi * hbar)) * integral
