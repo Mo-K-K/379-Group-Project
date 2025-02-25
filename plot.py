7dad0b89737b17fd7d4abc8bb45b54bb5448e96e
Mo Khan <60451707+Mo-K-K@users.noreply.github.com>
2025-02-25 19:28:44 +0000

    added the data generation

--- plot.py
+++ plot.py
@@ -1,3 +1,11 @@
+theta_vals = np.linspace(-3, 3, 50)
+p_vals = np.linspace(-3, 3, 50)
+
+wigner_vals = np.zeros((50, 50))
+for i, theta in enumerate(theta_vals):
+    for j, p in enumerate(p_vals):
+        wigner_vals[i, j] = wigner_function(0, theta, p)
+
 plt.figure(figsize=(8, 6))
 plt.contourf(theta_vals, p_vals, wigner_vals.T, levels=100, cmap='RdBu')
 plt.colorbar(label='Wigner function')
